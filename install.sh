de582e8fb7bac712720fe6e1372a861b5cbfddb5
rfrutos <roberto.frutos@keepler.io>
2020-10-12 11:44:46 +0200

    (dev) add pwsh

--- install.sh
+++ install.sh
@@ -123,12 +123,12 @@ add-apt-repository universe
 apt-get install -y powershell
 
 log_info "installing Docker"
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt-get update
 apt-cache policy docker-ce
-apt-get install docker-ce
+apt-get install docker-ce -y
 systemctl status docker
 usermod -aG docker ${SYSTEM_USER}
 
